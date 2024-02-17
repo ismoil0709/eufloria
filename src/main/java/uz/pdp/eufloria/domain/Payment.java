0997d33363179e6dfe27c625aa5339242af95505
escobar <erhardo2002@mail.ru>
2024-02-17 15:50:16 +0500

    EUF-7-create-crud-service-with-repo-payment

---
+++ src/main/java/uz/pdp/eufloria/domain/Payment.java
@@ -0,0 +1,31 @@
+package uz.pdp.eufloria.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String card;
+
+    private BigDecimal amount;
+//
+//    @OneToOne
+//    private Order order;
+}
