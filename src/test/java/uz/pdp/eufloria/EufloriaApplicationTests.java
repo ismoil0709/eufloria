92fe01f874b3902ef0d4c573cd2093ef10fb3147
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-17 14:54:30 +0500

    first commit

---
+++ src/test/java/uz/pdp/eufloria/EufloriaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.eufloria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EufloriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
