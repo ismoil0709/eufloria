aa2062bf353d0804f01bba216bbcf4ebfd6ef9f9
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-17 16:51:39 +0500

    Spring security added

---
+++ src/main/java/uz/pdp/eufloria/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package uz.pdp.eufloria.security;
+
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain filterChain(HttpSecurity http){
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(registry->registry.anyRequest().permitAll());
+        return http.build();
+    }
+}
