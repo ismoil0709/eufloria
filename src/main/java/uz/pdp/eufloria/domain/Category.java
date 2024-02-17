c4e43df8813b354ad0548435072d356d0af9aa0d
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-17 23:31:13 +0500

    removed wild cards

--- src/main/java/uz/pdp/eufloria/domain/Category.java
+++ src/main/java/uz/pdp/eufloria/domain/Category.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
