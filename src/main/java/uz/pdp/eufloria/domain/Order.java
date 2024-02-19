fd847e9c65f0034015a96bb7be91dc2691d1710f
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-19 22:23:33 +0500

    removed wild card

--- src/main/java/uz/pdp/eufloria/domain/Order.java
+++ src/main/java/uz/pdp/eufloria/domain/Order.java
@@ -1,6 +1,12 @@
 package uz.pdp.eufloria.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
