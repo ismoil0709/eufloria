ff0249af47cc023722ce6a8c8196a0a33a24c313
ismoil_0709 <abduganiyev.ismoil001@gmail.com>
2024-02-20 17:54:26 +0500

    Cors config added

--- src/main/java/uz/pdp/eufloria/security/SecurityConfiguration.java
+++ src/main/java/uz/pdp/eufloria/security/SecurityConfiguration.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableMethodSecurity
@@ -15,7 +20,18 @@ public class SecurityConfiguration {
     @SneakyThrows
     public SecurityFilterChain filterChain(HttpSecurity http){
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(c->c.configurationSource(corsConfigurationSource()));
         http.authorizeHttpRequests(registry->registry.anyRequest().permitAll());
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return source;
     }
 }
