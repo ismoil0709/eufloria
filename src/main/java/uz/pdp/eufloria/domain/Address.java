b700e803135813194b5c5fef4406cc4e6a44d793
ali-2244 <aliakbar7711@icloud.com>
2024-02-20 12:15:14 +0500

    address entity completed

---
+++ src/main/java/uz/pdp/eufloria/domain/Address.java
@@ -0,0 +1,31 @@
+package uz.pdp.eufloria.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String country;
+    private String city;
+    private String district;
+    private String street;
+    private String home;
+    @OneToOne
+    private User user;
+    private String lon;
+    private String lan;
+}
