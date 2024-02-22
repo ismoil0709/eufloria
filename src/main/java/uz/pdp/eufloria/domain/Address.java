0a73b7e3daef8b10ed17c426c2675f3d8911f3c1
ali-2244 <aliakbar7711@icloud.com>
2024-02-22 15:28:06 +0500

    address CRUD repo added

--- src/main/java/uz/pdp/eufloria/domain/Address.java
+++ src/main/java/uz/pdp/eufloria/domain/Address.java
@@ -5,7 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
